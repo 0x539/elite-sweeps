b6cd9c77b905d1572f7c1ea38e62ff037dadf1ad
Aaron -.- <omgbloodypass@gmail.com>
2017-01-07 22:16:43 +0000

    After all steps participants and options are displayed

--- run.py
+++ run.py
@@ -6,6 +6,9 @@ class Thing:
         self.name = name
         pass
 
+    def __str__(self):
+        return self.name
+
 
 class SweepChooser:
     def __init__(self):
@@ -31,6 +34,11 @@ class Greeter:
         print("**********************************************")
 
 
+def output_list(_list):
+    for x in _list:
+        print x
+
+
 class Program:
     def __init__(self):
         self._step = 0
@@ -38,30 +46,35 @@ class Program:
         pass
 
     def ask_for_participant(self):
-        print("Enter participant name or N to go to next step: ")
+        print("Enter participant name or nothing to go to next step: ")
         _in = raw_input()
         self._chooser.add_participant(_in)
-        if _in == "N":
+        if _in == "":
             self._step += 1
 
     def ask_for_option(self):
-        print("Enter option name or N to go to next step: ")
+        print("Enter option name or nothing to go to next step: ")
         _in = raw_input()
         self._chooser.add_option(_in)
-        if _in == "N":
+        if _in == "":
             self._step += 1
 
-    def run(self):
+    def clear(self):
         os.system("cls")
-
         Greeter.greet()
 
+    def run(self):
         while True:
+            self.clear()
             if self._step == 0:
                 self.ask_for_participant()
             elif self._step == 1:
                 self.ask_for_option()
             else:
+                print("Participants: ")
+                output_list(self._chooser.participants)
+                print("Options: ")
+                output_list(self._chooser.options)
                 break
 
 
