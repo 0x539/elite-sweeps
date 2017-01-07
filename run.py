35326f8cef89c2edafdc19037990bec7710982b8
Aaron -.- <omgbloodypass@gmail.com>
2017-01-07 22:40:44 +0000

    Last step now generates the selections for the sweep stake

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
 import os
+import random
+import string
 
 
 class Thing:
@@ -9,11 +11,15 @@ class Thing:
     def __str__(self):
         return self.name
 
+    def __repr__(self):
+        return self.name
+
 
 class SweepChooser:
     def __init__(self):
         self.participants = []
         self.options = []
+        self.selections = []
         pass
 
     def add_participant(self, name):
@@ -21,6 +27,11 @@ class SweepChooser:
 
     def add_option(self, name):
         self.options.append(Thing(name))
+
+    def match_participants(self):
+        random.shuffle(self.participants)
+        random.shuffle(self.options)
+        self.selections = zip(self.participants, self.options)
 
 
 class Greeter:
@@ -34,8 +45,8 @@ class Greeter:
         print("**********************************************")
 
 
-def output_list(_list):
-    for x in _list:
+def output_things(_things):
+    for x in _things:
         print x
 
 
@@ -48,16 +59,18 @@ class Program:
     def ask_for_participant(self):
         print("Enter participant name or nothing to go to next step: ")
         _in = raw_input()
-        self._chooser.add_participant(_in)
         if _in == "":
             self._step += 1
+        else:
+            self._chooser.add_participant(_in)
 
     def ask_for_option(self):
         print("Enter option name or nothing to go to next step: ")
-        _in = raw_input()
-        self._chooser.add_option(_in)
+        _in = string.strip(raw_input())
         if _in == "":
             self._step += 1
+        else:
+            self._chooser.add_option(_in)
 
     def clear(self):
         os.system("cls")
@@ -72,9 +85,12 @@ class Program:
                 self.ask_for_option()
             else:
                 print("Participants: ")
-                output_list(self._chooser.participants)
-                print("Options: ")
-                output_list(self._chooser.options)
+                output_things(self._chooser.participants)
+                print("\nOptions: ")
+                output_things(self._chooser.options)
+                self._chooser.match_participants()
+                print("\n`Selections: ")
+                output_things(self._chooser.selections)
                 break
 
 
